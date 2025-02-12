e47a16dc653c4ad03b52cba5f5487be6fad6e641
Umidjon Xusanov <161422490+Umidjon-24@users.noreply.github.com>
2025-02-12 22:28:00 +0500

    Update numeric.py

--- lesson-2/numeric.py
+++ lesson-2/numeric.py
@@ -1,1 +1,32 @@
+# 1
+a = float(input('Enter a number: '))
+print(round(a, 2))
 
+# 2
+a, b, c = int(input('Enter a number: ')), int(input('Enter a number: ')), int(input('Enter a number: '))
+print("Largest number:", max(a,b,c),"Smallest number:", min(a,b,c))
+
+#3
+kilometers = float(input("Enter a kilometers: "))
+print(kilometers*1000, "meters")
+print(kilometers*100_000, "centimeters")
+
+#4
+a,b = int(input('Enter first number: ')),int(input('Enter second number: '))
+print("Integer division is:", max(a,b)//min(a,b), "The remainder is:", max(a,b)%min(a,b))
+
+#5
+celcius = float(input("Temperature in Celcius: "))
+fahrenheit = (9/5)*celcius+32
+print(fahrenheit)
+
+#6
+number = int(input('Enter a number: '))
+print("The last digit of the number is {}".format(number%10))
+
+#7
+num = int(input('Enter a number: '))
+if (num%2 == 0):
+    print('Even')
+else:
+    print('Odd')
